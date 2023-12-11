f835875f5b00c41a233bd82a58a6401c4f71818b
Nafees Hassan <nafeeshassan16@gmail.com>
2023-12-11 08:30:48 +0500

    mutant productDao (addProduct always null)

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/dao/productDao.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/dao/productDao.java
@@ -19,22 +19,23 @@ public class productDao {
 
 	@Autowired
     private SessionFactory sessionFactory;
-	
+
 	public void setSessionFactory(SessionFactory sf) {
         this.sessionFactory = sf;
     }
-	
+
 	@Transactional
 	public List<Product> getProducts(){
 		return this.sessionFactory.getCurrentSession().createQuery("from PRODUCT").list();
 	}
-	
+
 	@Transactional
 	public Product addProduct(Product product) {
 		this.sessionFactory.getCurrentSession().save(product);
-		return product;
+		return null;
+		//return product;
 	}
-	
+
 	@Transactional
 	public Product getProduct(int id) {
 		return this.sessionFactory.getCurrentSession().get(Product.class, id);
